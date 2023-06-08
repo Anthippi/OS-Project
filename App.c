eaa258e32cbe636ca2bcda36ffcde080ca1f4fa8
Anthippi <anthi0401>
2023-06-08 16:17:08 +0300

    try

--- App.c
+++ App.c
@@ -2,21 +2,20 @@
 #include "define.h"
 #include <time.h>
 
-pthread_mutex_t lockCook;
-pthread_cond_t condCook;
-pthread_mutex_t lockOven;
-pthread_cond_t condOven;
-pthread_mutex_t lockPack;
-pthread_cond_t condPack;
-pthread_mutex_t lockDeliver;
-pthread_cond_t condDeliver;
+pthread_mutex_t lockCook, lockOven, lockPack, lockDeliver;
+pthread_cond_t condCook, condOven, condPack, condDeliver;
 
 int NumOfCooks = Ncook;
 int NumOfOvens = Noven;
 int NumOfPackers = Npacker;
 int NumOfDelivers = Ndeliverer;
 
-int income = 0;
+int total_income = 0;
+int plain_sales = 0, special_sales = 0;
+int successful_orders = 0, failed_orders = 0;
+int max_service_time = 0, total_service_time = 0;
+int max_cooling_time = 0,  total_cooling_time = 0;
+int seed;
 
 struct timespec waiting_for_ordering;
 struct timespec payment_time;
@@ -24,4 +23,7 @@ struct timespec pizza_preparation_time;
 struct timespec baking_time;
 struct timespec delivering_time;
 
+void *order(void *x){
+    int order_number = *(int *)x;
 
+}
