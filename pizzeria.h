30cf46eb53dd2e36f418dfcccc34d5195ee13fe6
Anthippi <59541666+Anthippi@users.noreply.github.com>
2025-03-21 22:53:52 +0200

    Create pizzeria.h

---
+++ pizzeria.h
@@ -0,0 +1,22 @@
+#ifndef UNTITLED_OK_H
+#define UNTITLED_OK_H
+const int Ncook = 2; // παρασκευαστές
+const int Noven = 15; // φούρνοι
+const int Npacker = 2; // υπάλληλοι πακεταρίσματος
+const int Ndeliverer = 10; // διανομείς
+const int Torderlow = 1; // λεπτό
+const int Torderhigh = 3; // λεπτά
+const int Norderlow = 1; // πίτσες
+const int Norderhigh = 5; // πίτσες
+const double Pplain = 0.6; // 60%
+const int Tpaymentlow = 1; // λεπτό
+const int Tpaymenthigh = 3; // λεπτά
+const double Pfail = 0.1; // 10%
+const int Cplain = 10; // ευρώ
+const int Cspecial = 12; // ευρώ
+const int Tprep = 1; // λεπτό
+const int Tbake = 10; // λεπτά
+const int Tpack = 1; // λεπτό
+const int Tdellow = 5; // λεπτά
+const int Tdelhigh = 15; // λεπτά
+#endif // UNTITLED_OK_H
